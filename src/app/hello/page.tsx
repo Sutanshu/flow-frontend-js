0a1b5b02bc1b173662366427e4f4cb458101e0b4
Sutanshu <sutanshu@ualberta.ca>
2023-12-10 22:05:06 -0700

    Refactor data fetching and display logic

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -2,20 +2,33 @@
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
+interface Result {
+  data: Item[];
+}
+
+interface Item {
+  age: number;
+  name: string;
+}
+let totalTime: number = 0;
 const fetchData = async () => {
+  let initialTime = performance.now();
   const res = await fetch("http://127.0.0.1:5000/api", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
+      "cache-control": "no-cache",
     },
   });
-  const result = await res.json();
-  console.log(result);
+  let finalTime = performance.now();
+  totalTime += finalTime - initialTime;
+  const result: Result = await res.json();
+  console.log(result?.data);
   return result?.data;
 };
 
 export default function Hello() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState<Item[]>();
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     if (loading) {
@@ -30,7 +43,8 @@ export default function Hello() {
     <main className={styles.main}>
       <div className={styles.description}>
         {loading && <p>Loading...</p>}
-        <p>{data}</p>
+        {!loading && data?.map((item: Item) => <p>{item?.age}</p>)}
+        {!loading && <p>Time: {totalTime} ms</p>}
       </div>
     </main>
   );
